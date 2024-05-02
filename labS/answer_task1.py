af821b82ace1cab9f50fdb0e67f70fde32f35368
LvXintao <807361189@qq.com>
2024-05-02 11:00:26 +0800

    Finish LabS

--- labS/answer_task1.py
+++ labS/answer_task1.py
@@ -18,5 +18,23 @@ def skinning(joint_translation, joint_orientation, T_pose_joint_translation, T_p
     vertex_translation = T_pose_vertex_translation.copy()
     
     #---------------你的代码------------------#
-    
+
+    # # Get the local coordinate of each vertex
+    # The looping version is too slow!! (3.0 fps)
+    # for vertex_id in range(skinning_idx.shape[0]):
+    #     skinning_idx_vertex=skinning_idx[vertex_id]
+    #     skinning_weight_vertex=skinning_weight[vertex_id] # (4,)
+    #     T_pose_vertex_position=T_pose_vertex_translation[vertex_id]
+    #     T_pose_joint_position=T_pose_joint_translation[skinning_idx_vertex]
+    #     # Get the local coordinate of each vertex
+    #     T_pose_vertex_local=T_pose_vertex_position-T_pose_joint_position
+    #     # Get the position in the world coordinate
+    #     global_position=R.from_quat(joint_orientation[skinning_idx_vertex]).apply(T_pose_vertex_local)+joint_translation[skinning_idx_vertex] # (4,3)
+    #     vertex_translation[vertex_id]=np.sum(global_position*skinning_weight_vertex[:,np.newaxis],axis=0)
+
+    # The Numpy Vectorized version is way much faster (60 fps or more)
+    T_pose_vertex_local=T_pose_vertex_translation[:,np.newaxis,:]-T_pose_joint_translation[skinning_idx] # (N,4,3)
+    global_position=R.from_quat(joint_orientation[skinning_idx].reshape(-1,4)).apply(T_pose_vertex_local.reshape(-1,3)).reshape(-1,4,3)+joint_translation[skinning_idx] # (N,4,3)
+    vertex_translation=np.sum(global_position*skinning_weight[:,:,np.newaxis],axis=1).squeeze() # (N,3)
+
     return vertex_translation
